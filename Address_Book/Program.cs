f21ce657685d66348d35951f89ca653d72d7f556
shwethab8888 <shwethab8888@gmail.com>
2023-03-02 17:33:39 +0530

    Creating contacts in Address book system

---
+++ Address_Book/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Address_Book
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Welcome to Address Book System");
+           CreateContacts.Contact contact = new CreateContacts.Contact();
+        
+        
+        }
+    }
+}
