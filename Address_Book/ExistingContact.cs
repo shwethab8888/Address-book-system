2bea073681e09a7890333b1759c455e15cfd9943
shwethab8888 <122897911+shwethab8888@users.noreply.github.com>
2023-03-11 00:14:03 +0530

    Update ExistingContact.cs

--- Address_Book/ExistingContact.cs
+++ Address_Book/ExistingContact.cs
@@ -1,83 +1,68 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using static Address_Book.AddressBookMain;
 
 namespace Address_Book
 {
-    public class ExistingContact
+    public class EditContact
     {
-        public static void Existing_Contact()
-        {
-            static void Main(string[] args)
+
+            private List<CreateContact> _contacts;
+
+            public EditContact(List<CreateContact> contacts)
             {
-                // Create a new address book
-                AddressBookMain addressBook = new AddressBookMain();
+                _contacts = contacts;
+            }
 
-                // Add some contacts
-                addressBook.AddContact(new CreateContact("John", "Doe", "123 Main St", "Anytown", "CA", "12345", "555-555-5555", "johndoe@email.com"));
-                addressBook.AddContact(new CreateContact("Jane", "Doe", "456 Elm St", "Anytown", "CA", "12345", "555-555-5555", "janedoe@email.com"));
-
-                // Display all contacts in the address book
-                Console.WriteLine("All Contacts:");
-                addressBook.PrintContacts();
-
-                // Prompt the user to enter the first and last name of the contact to update
-                Console.Write("Enter the first name of the contact to update: ");
+            public void EditContactDetails()
+            {
+                Console.Write("Enter the first name of the contact to edit: ");
                 string firstName = Console.ReadLine();
-                Console.Write("Enter the last name of the contact to update: ");
+
+                Console.Write("Enter the last name of the contact to edit: ");
                 string lastName = Console.ReadLine();
 
-                // Find the contact by name
-                CreateContact contact = addressBook.FindContactByName(firstName, lastName);
+                // Find the contact to edit by first and last name
+                CreateContact contactToEdit = _contacts.Find(c => c.FirstName == firstName && c.LastName == lastName);
 
-                // If the contact is not found, display an error message and exit
-                if (contact == null)
+                if (contactToEdit != null)
                 {
-                    Console.WriteLine("Contact not found.");
-                    return;
+                    Console.WriteLine($"Enter new details for {contactToEdit.FirstName} {contactToEdit.LastName}:");
+                    Console.Write("New Address: ");
+                    string address = Console.ReadLine();
+
+                    Console.Write("New City: ");
+                    string city = Console.ReadLine();
+
+                    Console.Write("New State: ");
+                    string state = Console.ReadLine();
+
+                    Console.Write("New Zip: ");
+                    string zip = Console.ReadLine();
+
+                    Console.Write("New Phone Number: ");
+                    string phone = Console.ReadLine();
+
+                    Console.Write("New Email: ");
+                    string email = Console.ReadLine();
+
+                    // Update the contact's details
+                    contactToEdit.Address = address;
+                    contactToEdit.City = city;
+                    contactToEdit.State = state;
+                    contactToEdit.Zip = zip;
+                    contactToEdit.Phone = phone;
+                    contactToEdit.Email = email;
+
+                    Console.WriteLine("Contact details updated successfully!");
                 }
-
-                // Prompt the user to update the contact's information
-                Console.Write("Enter the new address: ");
-                string address = Console.ReadLine();
-                Console.Write("Enter the new city: ");
-                string city = Console.ReadLine();
-                Console.Write("Enter the new state: ");
-                string state = Console.ReadLine();
-                Console.Write("Enter the new zip code: ");
-                string zip = Console.ReadLine();
-                Console.Write("Enter the new phone number: ");
-                string phoneNumber = Console.ReadLine();
-                Console.Write("Enter the new email: ");
-                string email = Console.ReadLine();
-
-                // Update the contact's information
-                contact.Address = address;
-                contact.City = city;
-                contact.State = state;
-                contact.Zip = zip;
-                contact.PhoneNumber = phoneNumber;
-                contact.Email = email;
-
-                // Display the updated contact information
-                Console.WriteLine("Contact updated:");
-                Console.WriteLine("Name: " + contact.FirstName + " " + contact.LastName);
-                Console.WriteLine("Address: " + contact.Address);
-                Console.WriteLine("City: " + contact.City);
-                Console.WriteLine("State: " + contact.State);
-                Console.WriteLine("Zip: " + contact.Zip);
-                Console.WriteLine("Phone Number: " + contact.PhoneNumber);
-                Console.WriteLine("Email: " + contact.Email);
-
-                // Display all contacts in the address book after the update
-                Console.WriteLine("All Contacts:");
-                addressBook.PrintContacts();
-
-                Console.ReadLine();
+                else
+                {
+                    Console.WriteLine($"Could not find a contact with the name {firstName} {lastName}");
+                }
             }
         }
     
 
-    }
 }
